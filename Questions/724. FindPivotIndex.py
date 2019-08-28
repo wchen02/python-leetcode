c1891ed96283b08fd5df09596e269a34b447a8f7
Wensheng Chen <hi@wenshengchen.com>
2019-08-28 01:43:58 -0700

    feat: Solves Find Pivot Index

---
+++ Questions/724. FindPivotIndex.py
@@ -0,0 +1,72 @@
+from typing import List, Dict
+
+"""
+Given an array of integers `nums`, write a method that returns the "pivot" index of this array.
+
+We define the pivot index as the index where the sum of the numbers to the left of the index is equal to the sum of the numbers to the right of the index.
+
+If no such index exists, we should return -1. If there are multiple pivot indexes, you should return the left-most pivot index.
+
+**Example 1**:
+> **Input**: 
+> nums = [1, 7, 3, 6, 5, 6]
+> **Output**: 3
+> **Explanation**: 
+> The sum of the numbers to the left of index 3 (nums[3] = 6) is equal to > the sum of numbers to the right of index 3.
+> Also, 3 is the first index where this occurs.
+
+**Example 2**:
+> **Input**: 
+> nums = [1, 2, 3]
+> **Output**: -1
+> Explanation: 
+> There is no index that satisfies the conditions in the problem statement.
+
+**Note**:
+
+- The length of `nums` will be in the range `[0, 10000]`.
+- Each element `nums[i]` will be an integer in the range `[-1000, 1000]`.
+"""
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left = []
+        right = []
+
+        rightSum = 0
+        for idx, num in enumerate(nums[::-1]):
+            right.append(rightSum)
+            rightSum += num
+
+        leftSum = 0
+        for idx, num in enumerate(nums):
+            left.append(leftSum)
+            leftSum += num
+            if left[idx] == right[len(nums)-1-idx]:
+                return idx
+
+        return -1
+
+## Test Cases
+test = Solution()
+answer = test.pivotIndex([1,2,3,4,6])
+assert answer == 3
+answer = test.pivotIndex([-1,1,-2,2,3,4,-4])
+assert answer == 4
+answer = test.pivotIndex([-1,1,-2,2,-3,3,4,-4])
+assert answer == -1
+answer = test.pivotIndex([-1,-1,0,0,-1,-1])
+assert answer == 2
+answer = test.pivotIndex([1,2,1])
+assert answer == 1
+answer = test.pivotIndex([1,2])
+assert answer == -1
+answer = test.pivotIndex([1])
+assert answer == 0
+answer = test.pivotIndex([])
+assert answer == -1
+print('All Passed!')
+
+## Big O Analysis
+# **Space Complexity**: O(N)
+
+# **Time Complexity**: O(N)
