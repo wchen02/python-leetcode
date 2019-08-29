f46b62f5d7ed3eb510db260a5a38398033fc8257
Wensheng Chen <hi@wenshengchen.com>
2019-08-29 00:18:43 -0700

    Remove comments and link to the website instead

--- Questions/1-TwoSum.py
+++ Questions/1-TwoSum.py
@@ -1,22 +1,6 @@
 from typing import List, Dict
 
-"""
-1. Two Sum
-**Easy**
-
-Given an array of integers, return **indices** of the two numbers such that they add up to a specific target.
-
-You may assume that each input would have ***exactly*** one solution, and you may not use the *same* element twice.
-
-**Example**:
-
-Given nums = [2, 7, 11, 15], target = 9,
-
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
-"""
-
-## Solution
+# See details here https://wenshengchen.com/2019/08/27/1-two-sum.html
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dict: Dict[int, int] = {}
@@ -43,8 +27,3 @@ assert answer == [0,6]
 answer = test.twoSum([3,2,4], 6)
 assert answer == [1,2]
 print('All Passed!')
-
-## Big O Analysis
-# **Space Complexity**: O(N)
-
-# **Time Complexity**: O(N)
--- Questions/724-FindPivotIndex.py
+++ Questions/724-FindPivotIndex.py
@@ -1,32 +1,6 @@
-from typing import List, Dict
+from typing import List
 
-"""
-Given an array of integers `nums`, write a method that returns the "pivot" index of this array.
-
-We define the pivot index as the index where the sum of the numbers to the left of the index is equal to the sum of the numbers to the right of the index.
-
-If no such index exists, we should return -1. If there are multiple pivot indexes, you should return the left-most pivot index.
-
-**Example 1**:
-> **Input**: 
-> nums = [1, 7, 3, 6, 5, 6]
-> **Output**: 3
-> **Explanation**: 
-> The sum of the numbers to the left of index 3 (nums[3] = 6) is equal to > the sum of numbers to the right of index 3.
-> Also, 3 is the first index where this occurs.
-
-**Example 2**:
-> **Input**: 
-> nums = [1, 2, 3]
-> **Output**: -1
-> Explanation: 
-> There is no index that satisfies the conditions in the problem statement.
-
-**Note**:
-
-- The length of `nums` will be in the range `[0, 10000]`.
-- Each element `nums[i]` will be an integer in the range `[-1000, 1000]`.
-"""
+# See details here https://wenshengchen.com/2019/08/28/724-find-pivot-index.html
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         rightSum = sum(nums)
@@ -58,8 +32,3 @@ assert answer == 0
 answer = test.pivotIndex([])
 assert answer == -1
 print('All Passed!')
-
-## Big O Analysis
-# **Space Complexity**: O(1)
-
-# **Time Complexity**: O(N)
