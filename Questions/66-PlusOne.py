fbbe5ea10d537c44dcff4e4d4bfe8d71e541f350
Wensheng Chen <hi@wenshengchen.com>
2019-10-02 01:04:56 -0700

    refactor: improve plus one solution

--- Questions/66-PlusOne.py
+++ Questions/66-PlusOne.py
@@ -3,16 +3,15 @@ from typing import List
 # See details here # See details here https://wenshengchen.com/2019/08/29/66-plus-one.html
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        digits[len(digits) - 1] += 1
-
         for i in range(len(digits) - 1, -1, -1):
-            if digits[i] > 9:
-                digits[i] %= 10
-                if (i - 1 < 0):
-                    digits.insert(0, 1)
-                else:
-                    digits[i - 1] += 1
-
+            if (digits[i] < 9):
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+        
+        digits[0] = 1
+        digits.append(0)
         return digits
 
 ## Test Cases
