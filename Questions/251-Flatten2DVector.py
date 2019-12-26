4cc807c71a1331f54a11b87a5563864a4ff0e5a9
Wensheng Chen <hi@wenshengchen.com>
2019-12-25 16:42:14 -0800

    feat: Solves Fallten 2d vector

--- Questions/251-Flatten2DVector.py
+++ Questions/251-Flatten2DVector.py
@@ -1,31 +1,51 @@
 from typing import List
 
-# See details here https://wenshengchen.com/2019/12/04/412-fizz-buzz.html
+# See details here https://wenshengchen.com/2019/12/21/251-flatten-2d-vector.html
 class Vector2D:
-    ptr, stk, v
-
     def __init__(self, v: List[List[int]]):
-        self.ptr = 0
-        self.stk = [v[ptr]]
+        self.j = -1
+        self.i = 0
         self.v = v
 
     def next(self) -> int:
         self.hasNext()
+        self.j += 1
+        return self.v[self.i][self.j]
 
     def hasNext(self) -> bool:
-        if not stk:
-            if ptr < len(v) - 1:
-                ptr += 1
-
+        if self.i >= len(self.v):
+            return False
+        
+        if self.j+1 >= len(self.v[self.i]):
+            self.i += 1
+            self.j = -1
+            return self.hasNext()
+        else:
+            return True
 
 ## TEST CASES
-Vector2D iterator = Vector2D([[1,2],[3],[4]]);
-
+iterator = Vector2D([[1,2],[3],[4]])
 assert iterator.next() == 1
 assert iterator.next() == 2
 assert iterator.next() == 3
 assert iterator.hasNext() == True
 assert iterator.hasNext() == True
 assert iterator.next() == 4
+assert iterator.hasNext() == False
+
+iterator = Vector2D([[],[1]])
+assert iterator.next() == 1
+assert iterator.hasNext() == False
+
+iterator = Vector2D([[1],[]])
+assert iterator.next() == 1
+assert iterator.hasNext() == False
+
+iterator = Vector2D([[1,2],[],[3]])
+assert iterator.hasNext() == True
+assert iterator.hasNext() == True
+assert iterator.next() == 1
+assert iterator.next() == 2
+assert iterator.next() == 3
 assert iterator.hasNext() == False
 print('All Passed!')
