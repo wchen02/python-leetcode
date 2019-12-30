36857321e2178e823aa895fea5b2d7ae66977a96
Wensheng Chen <hi@wenshengchen.com>
2019-12-29 17:56:22 -0800

    feat: Solves Find Peak Element

---
+++ Questions/162-FindPeakElement.py
@@ -0,0 +1,55 @@
+from typing import List
+import sys
+
+# See details here https://wenshengchen.com/2019/12/29/162-find-peak-element.html
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        if not nums:
+            return -1
+
+        lo, hi = 0, len(nums)-1
+        
+        while lo+1 < hi:
+            mid = lo + (hi-lo)//2
+            left = nums[mid-1]
+            right = nums[mid+1]
+            
+            if left < nums[mid] and nums[mid] > right:
+                return mid
+            elif left < nums[mid] and nums[mid] < right:
+                lo = mid
+            else:
+                hi = mid
+        
+
+        for i in [lo, hi]:
+            left = nums[i-1] if i - 1 >= 0 else -sys.maxsize-1
+            right = nums[i+1] if i + 1 < len(nums) else -sys.maxsize-1
+            if left < nums[i] and nums[i] > right:
+                return i
+
+        return -1
+
+## TEST CASES
+test = Solution()
+answer = test.findPeakElement([1,2,3,1])
+assert answer == 2, answer
+
+answer = test.findPeakElement([1,2,3,4])
+assert answer == 3, answer
+
+answer = test.findPeakElement([8,7,6,5])
+assert answer == 0, answer
+
+answer = test.findPeakElement([9])
+assert answer == 0, answer
+
+answer = test.findPeakElement([])
+assert answer == -1, answer
+
+answer = test.findPeakElement([7,7])
+assert answer == -1, answer
+
+answer = test.findPeakElement([1,2,1,3,5,6,4])
+assert answer == 1 or answer == 5, answer
+print('All Passed!')
