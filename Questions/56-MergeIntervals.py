b00ad2702a2f075f983380516125d36feedf0ce6
Wensheng Chen <hi@wenshengchen.com>
2020-01-04 17:06:21 -0800

    test: added empty test case

--- Questions/56-MergeIntervals.py
+++ Questions/56-MergeIntervals.py
@@ -19,4 +19,5 @@ class Solution:
 test = Solution()
 assert test.merge([[1,3],[2,6],[8,10],[15,18]]) == [[1,6],[8,10],[15,18]]
 assert test.merge([[1,4],[4,5]]) == [[1,5]]
+assert test.merge([]) == []
 print('All Passed!')
