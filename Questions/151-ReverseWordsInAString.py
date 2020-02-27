ad46cb5b8006a1484d64fabe66a18a1b9971f9c4
Wensheng Chen <hi@wenshengchen.com>
2020-02-26 21:56:09 -0500

    feat: solves reverse words in a string

---
+++ Questions/151-ReverseWordsInAString.py
@@ -0,0 +1,26 @@
+from typing import List, Set
+
+# See details here https://wenshengchen.com/2020/02/24/981-time-based-key-value-store.html
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.split(" ")
+        
+        res = []
+        for i in range(len(words)-1, -1, -1):
+            if words[i]:
+                res.append(words[i])
+        
+        return " ".join(res)
+
+## TEST CASES
+test = Solution()
+assert test.reverseWords("the sky is blue") == "blue is sky the"
+assert test.reverseWords("  hello world!  ") == "world! hello"
+assert test.reverseWords("a good   example") == "example good a"
+print('All Passed!')
